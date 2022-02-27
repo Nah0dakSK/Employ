a88f66fefec7ee8456df43855a8237a6f67c4f5e
Nah0dakSK <71822901+Nah0dakSK@users.noreply.github.com>
2022-02-27 23:45:17 +0100

    add
    now

--- employ/src/App.js
+++ employ/src/App.js
@@ -1,8 +1,12 @@
 function App() {
   return (
     <div>
+      <p style={{color: "#504F76", backgroundColor: "#504F76", fontSize: "20px"}}>topline</p>
+      <h1 style={{color: "#3688A3", fontFamily: "Arial Black", 
+                  textShadow: "2px 2px 0px  #000, -2px -2px 0px  #000, 2px -2px 0px  #000, -2px 2px 0px  #000;", 
+                  marginTop: "225px", textAlign: "center"}}>JOBSIDE</h1>
+      <img src="./BackImage.jpg" alt="background" />            
     </div>
   );
 }
-
 export default App;
